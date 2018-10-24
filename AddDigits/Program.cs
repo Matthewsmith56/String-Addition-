932b4b1140f493803d3d46ada1a0e28cbca0aa66
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-24 09:47:34 -0400

    FInal Program

--- AddDigits/Program.cs
+++ AddDigits/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AddDigits
 {
@@ -10,6 +8,38 @@ namespace AddDigits
     {
         static void Main(string[] args)
         {
+            List<int> inputList = new List<int>();
+            int IntegerSum = 0;
+
+            Console.WriteLine("Enter an integer");
+
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            while (inputList.Count != 1)
+            {
+                while (userInput > 0)
+                {
+                    inputList.Add(userInput % 10);
+                    userInput = userInput / 10;
+                }
+
+                foreach (var i in inputList)
+                {
+                    IntegerSum += i;
+                }
+
+                Console.WriteLine(IntegerSum);
+
+                inputList.Clear();
+
+                while (IntegerSum > 0)
+                {
+                    inputList.Add(IntegerSum % 10);
+                    IntegerSum = IntegerSum / 10;
+                }
+                
+                IntegerSum = 0;
+            }
         }
     }
 }
